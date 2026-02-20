cf7d85651d1215fab188ed9a3a921d6a5a6d2a8a
Justin <interjc@gmail.com>
2026-02-20 21:26:33 +0900

    fix cf remote build error

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,6 @@
 import createMiddleware from 'next-intl/middleware';
 import { routing } from './i18n/routing';
 
-// Edge runtime is required for OpenNext Cloudflare
-export const runtime = 'edge';
-
 export default createMiddleware(routing);
 
 export const config = {
